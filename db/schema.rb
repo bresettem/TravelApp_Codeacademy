9a6e087f17481ff57c5e519ef487a4b944484c30
bresettem <bresettem15@yahoo.com>
2018-02-09 01:51:26 +0000

    Destination controller with show page

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,18 @@
 ActiveRecord::Schema.define(version: 20180207011048) do
 
   create_table "destinations", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "description"
+    t.integer "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_destinations_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
